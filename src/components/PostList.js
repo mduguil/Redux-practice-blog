fe35bf812072330f4bb4311fe77a46aaffb23d82
Mikaela Duguil <kimmieduggz@gmail.com>
2021-07-09 20:14:21 -0700

    redux

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,14 +3,23 @@ import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
   render() {
+    console.log(this.props.posts)
     return (
       <div>PL</div>
     )
   }
 }
 
+const mapStateToProps = state => {
+  return { posts: state.posts };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
   )(PostList)
