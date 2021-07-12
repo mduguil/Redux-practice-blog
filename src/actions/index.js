76747e9c4f5dcba02e204fda2826d13bd77285fd
Mikaela Duguil <kimmieduggz@gmail.com>
2021-07-12 11:54:17 -0700

    rendering data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
-import jsonplaceholder from '../apis/jsonPlaceholder'
+import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonplaceholder.get('/posts')
+  const response = await jsonPlaceholder.get('/posts')
 
-  dispatchEvent({ type: 'FETCH_POSTS', payload: response })
+  dispatch({ type: 'FETCH_POSTS', payload: response.data })
 
 }
